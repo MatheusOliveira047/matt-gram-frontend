eca9d23457f047106e6db944c739024e63054885
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-01-02 11:52:01 -0300

    criando css global e page register

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,9 +1,29 @@
 import "./Auth.css"
 
+import { Link } from 'react-router-dom'
+
+import { useState,useEffect } from 'react'
+
+
 const Register = ()=>{
+  
+
+  const handleSubmitRegister = (e)=>{
+    e.preventDefault()
+  }
+  
   return(
     <div>
-      <h1>Register</h1>
+      <h2>Matt Gram</h2>
+      <p className='subtitle'> Cadastra-se para ver as fotos de seus</p>
+      <form onSubmit={handleSubmitRegister}>
+        <input type="text" placeholder='Nome' />
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Senha'/>
+        <input type="password" placeholder='Conforme a Senha'/>
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>Já possui conta ? <Link to={"/login"}>Clique Aqui</Link></p>
     </div>
   )
 }
